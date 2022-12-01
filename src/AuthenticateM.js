efc0d356323a64d74e392b1e19d1817565224232
Rishi Santhanam <rishisant@tamu.edu>
2022-11-30 20:00:05 -0600

    redir

--- src/AuthenticateM.js
+++ src/AuthenticateM.js
@@ -9,10 +9,34 @@ import './AuthenticateStyle.css';
 // import {raise_admin_bar} from './HomeFunctions';
 
 import {raise_admin_bar} from './HomeFunctions';
-import { isValidManager } from './AuthentScript';
+// import { isValidManager } from './AuthentScript';
 
 const AuthenticateM = () => {
     const navigate = useNavigate();
+    var validusers = ["rishisanthanam", "mattjuntunen", "esbenegholm", "nayabrehmat"];
+    var validpass = ["529009921", "630007600", "228007063", "528000730"];   
+
+    // create a function to check if the user is valid using validusers[i] and validpass[i]
+    // if the user is valid, then navigate to the manager page
+    // if the user is not valid, then print "Invalid Manager" to the console
+    function isValidManager() {
+        var isvalid = false;
+        var user_input = document.getElementById("username_field").value;
+        var pass_input = document.getElementById("password_field").value;
+        for (let i=0; i < validusers.length; ++i) {
+            if (user_input === validusers[i] && pass_input === validpass[i]) {
+                isvalid = true;
+            }
+        }
+        if (isvalid) {
+            console.log("Valid Manager");
+            // navigate to manager
+            navigate('/Manager');
+        }
+        else {
+            console.log("Invalid Manager");
+        }
+    }
     
     return (
         <div id="homecontainer">
--- src/AuthenticateS.js
+++ src/AuthenticateS.js
@@ -8,11 +8,35 @@ import './AuthenticateStyle.css';
 // import {raise_admin_bar} from './HomeFunctions';
 // import {raise_admin_bar} from './HomeFunctions';
 import {raise_admin_bar} from './HomeFunctions';
-import { isValidServer } from './AuthentScript';
+// import { isValidServer } from './AuthentScript';
 import { print_All_Vals, products, ingredients, prices } from './Home';
 
 const AuthenticateS = () => {
     const navigate = useNavigate();
+    var validusers = ["rishisanthanam", "mattjuntunen", "esbenegholm", "nayabrehmat"];
+    var validpass = ["529009921", "630007600", "228007063", "528000730"];   
+
+    // create a function to check if the user is valid using validusers[i] and validpass[i]
+    // if the user is valid, then navigate to the manager page
+    // if the user is not valid, then print "Invalid Manager" to the console
+    function isValidServer() {
+        var isvalid = false;
+        var user_input = document.getElementById("username_field").value;
+        var pass_input = document.getElementById("password_field").value;
+        for (let i=0; i < validusers.length; ++i) {
+            if (user_input === validusers[i] && pass_input === validpass[i]) {
+                isvalid = true;
+            }
+        }
+        if (isvalid) {
+            console.log("Valid Server");
+            // navigate to manager
+            navigate('/Server');
+        }
+        else {
+            console.log("Invalid Server");
+        }
+    }
     
     return (
         <div id="homecontainer">
